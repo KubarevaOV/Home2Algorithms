1a95b874530ff0e7431264ad61efde40fa062f7f
Dmitry Kuklin <dmi-kuklin@yandex.ru>
2022-11-30 20:18:25 +0300

    Home tasks 10

---
+++ Task10/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+var str = number.ToString();
+Console.WriteLine("Вторая цифра: {0}", str[1]);
