1ed9e407e2709fac6de3a28a939f94e232c0500b
Dmitry Kuklin <dmi-kuklin@yandex.ru>
2022-11-30 20:34:57 +0300

    Home tasks 15

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,11 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 Console.WriteLine("Введите любое число:");
 int number = Convert.ToInt32(Console.ReadLine());
 var str = number.ToString();
 
 if (str.Length >= 3) {
-    Console.WriteLine("Вторая цифра: {0}", str[2]);
+    Console.WriteLine("Третья цифра: {0}", str[2]);
 } else {
     Console.WriteLine("Третьей цифры нет");
 }
---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите номер дня на неделе:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number) {
+    case 1: case 2: case 3: case 4: case 5:
+        Console.WriteLine("Это не выходной. Работай");
+        break;
+    case 6: case 7:
+        Console.WriteLine("Выходной! Гуляем");
+        break;
+    default:
+        Console.WriteLine("Таких дней не бывает");
+        break;
+}
